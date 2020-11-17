f4e8dfd7944866a61f0d63287be21ff4ef838168
JP <jpjones22@gmail.com>
2020-11-16 20:05:37 -0800

    Created Error Page if error exists

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.get('/hello', (req, res) => {
 
 
 //Listening to the ether
+
 app.listen(PORT, () => {
   console.log(`Server is working on ${PORT}`);
 })
@@ -32,24 +33,44 @@ app.listen(PORT, () => {
 //Functions
 
 function showForm(req, res) {
-  res.render('pages/searches/new.ejs');
+  try { res.render('pages/searches/new.ejs'); }
+  catch (err) {
+    res.render('pages/error', errorRender(err));
+  }
+
 }
 
 function createSearch(req, res) {
-  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+  try {
+    let url = 'https://www.googleapis.com/books/v1/volu';//mes?q=';
 
-  if (req.body.search[1] === 'title') { url += `+intitle:${req.body.search[0]}`; }
+    if (req.body.search[1] === 'title') { url += `+intitle:${req.body.search[0]}`; }
 
-  if (req.body.search[1] === 'author') { url += `+inauthor:${req.body.search[0]}`; }
+    if (req.body.search[1] === 'author') { url += `+inauthor:${req.body.search[0]}`; }
 
-  superagent.get(url)
-    .then(data => {
-      return data.body.items.map(book => new Book(book.volumeInfo));
-    })
-    .then(results => {
-      res.render('pages/searches/show', { searchResults: JSON.stringify(results) });
-    })
-    .catch(err => console.error('ERROR getting book data:', err));
+    superagent.get(url)
+      .then(data => {
+        return data.body.items.map(book => new Book(book.volumeInfo));
+      })
+      .then(results => {
+        res.render('pages/searches/show', { searchResults: JSON.stringify(results) });
+      })
+      .catch(err => {
+        res.render('pages/error', errorRender(err));
+      });
+  }
+  catch (err) {
+    res.render('pages/error', errorRender(err));
+  }
+}
+
+function errorRender(err) {
+
+  console.log('ErRor FunCtIon', err);
+  return {
+    errorString: 'Your Request Cannot be processed',
+    error: err
+  };
 }
 
 function Book(info) {
@@ -61,7 +82,7 @@ function Book(info) {
 
     this.image = info.imageLinks.thumbnail : this.image = 'https://i.imgur.com/J5LVHEL.jpg');
 
-  if (this.image.substring(0,6) !== 'https') {
+  if (this.image.substring(0, 6) !== 'https') {
     let imageLinkString = this.image.substring(6);
     let imageUrl = 'https:' + imageLinkString;
     this.image = imageUrl;
