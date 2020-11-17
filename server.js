0ba1aa1cb9f110cad801283f2a872fe1e62dc273
JP <jpjones22@gmail.com>
2020-11-16 16:41:51 -0800

    complete User Story 2 - Book Query

--- server.js
+++ server.js
@@ -14,14 +14,44 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
-
 app.set('view engine', 'ejs');
 
-
+//Routes
+app.get('/searches/new', showForm);
+app.post('/searches', createSearch);
 app.get('/hello', (req, res) => {
   res.render('pages/index');
 })
 
+
+//Listening to the ether
 app.listen(PORT, () => {
   console.log(`Server is working on ${PORT}`);
 })
+
+//Functions
+
+function showForm(req, res) {
+  res.render('pages/searches/new.ejs');
+}
+
+function createSearch(req, res) {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+
+  if (req.body.search[1] === 'title') { url += `+intitle:${req.body.search[0]}`; }
+
+  if (req.body.search[1] === 'author') { url += `+inauthor:${req.body.search[0]}`; }
+
+  superagent.get(url)
+    .then(data => {
+      return data.body.items.map(book => new Book(book.volumeInfo));
+    })
+    .then(results => {
+      res.render('pages/searches/show', { searchResults: JSON.stringify(results) });
+    })
+    .catch(err => console.error('ERROR getting book data:', err));
+}
+
+function Book(info) {
+  this.title = info.title || 'No Title Available!  Check something else!';
+}
